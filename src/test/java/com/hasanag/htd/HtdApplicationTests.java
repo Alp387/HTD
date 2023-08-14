4f7d61534f855cadcc4bebc4c6dfc8f8b68110ef
HasanAlperenGürbüz <alperen87@hotmail.com>
2023-08-14 15:09:02 +0200

    Initial commit

---
+++ src/test/java/com/hasanag/htd/HtdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hasanag.htd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HtdApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
